7f6306b0fc9c6107ac4da268961c31f3d5c86d4f
Jacobmercier <jacob.mercier.2@ulaval.ca>
2025-10-11 17:17:48 -0400

    Update Chapitre2Probleme21.jl

--- Chapitre2Probleme21.jl
+++ Chapitre2Probleme21.jl
@@ -3,12 +3,12 @@ using Distributions, Statistics
 function simuler_naissance_mort(λ, μ, temps_total=20.0, n_sim=1000)
     populations_finales = Int64[]
     
-    for _ in 1:n_sim
+    for i in 1:n_sim
         population = 1
         temps = 0.0
         
         while temps < temps_total && population > 0
-            # Temps jusqu'au prochain événement
+        
             taux_total = population * (λ + μ)
             Δt = rand(Exponential(1/taux_total))
             
@@ -17,12 +17,11 @@ function simuler_naissance_mort(λ, μ, temps_total=20.0, n_sim=1000)
             end
             
             temps += Δt
-            
-            # Décider si naissance ou mort
+           
             if rand() < λ/(λ + μ)
-                population += 1  # Naissance
+                population += 1  
             else
-                population -= 1  # Mort
+                population -= 1  
             end
         end
         
@@ -32,15 +31,8 @@ function simuler_naissance_mort(λ, μ, temps_total=20.0, n_sim=1000)
     return populations_finales
 end
 
-# Simulation pour les trois cas
-println("PROCESSUS NAISSANCE-MORT (20 minutes)")
-println("="^50)
 
-cas_test = [
-    (1.00, 1.0),
-    (1.05, 1.0), 
-    (1.10, 1.0)
-]
+cas_test = [(1.00, 1.0), (1.05, 1.0), (1.10, 1.0)]
 
 for (λ, μ) in cas_test
     println("Simulation pour λ = $λ, μ = $μ...")
