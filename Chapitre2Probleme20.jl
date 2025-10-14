f692fa1f895a3917a1e89469b332a6309bc5467c
VincentLarochelle <151478437+VincentLarochelle@users.noreply.github.com>
2025-10-13 22:13:35 -0400

    Fin du travail

--- Chapitre2Probleme20.jl
+++ Chapitre2Probleme20.jl
@@ -2,19 +2,15 @@ using Distributions, Statistics
 
 function verifier_probabilite(λ, μ, n_sim=100000)
     compteur = 0
-    
     for _ in 1:n_sim
         T_λ = rand(Exponential(1/λ))
         T_μ = rand(Exponential(1/μ))
-        
         if T_λ < T_μ
             compteur += 1
         end
     end
-    
     prob_empirique = compteur / n_sim
     prob_theorique = λ / (λ + μ)
-    
     return prob_empirique, prob_theorique
 end
 
